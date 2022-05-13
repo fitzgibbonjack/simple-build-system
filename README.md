6b9f2f33b7f7b4417c9eac6c96acd7097f657a6a
Jack Fitzgibbon <jack.fitzgibbon@enginecreative.co.uk>
2022-05-13 19:46:59 +0100

    initial: First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Engine Creative - Web Build System
+
+## Overview
+
+A simple build system and file bundler for web based projects.
+
+## Installation
+
+### `npm install`
+
+Install Node dependencies.
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm run dev`
+
+Watches for changes in .scss files. On change, compiles to css within src directory.
+
+### `npm run build`
+
+Builds the project for production to the dist folder.
+SCSS is compiled and bundled along with Javascript. Final bundles are minified for the best performance.
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackTagsPlugin = require("html-webpack-tags-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+	mode: "production",
+	entry: path.resolve(__dirname, "src/js/index.js"),
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "main.min.js",
+		assetModuleFilename: "[name][ext]",
+	},
+	devtool: "source-map",
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "src/index.html",
+		}),
+		new HtmlWebpackTagsPlugin({ tags: ["main.min.css"] }),
+		new CopyPlugin({
+			patterns: [{ from: "src/assets", to: "assets", noErrorOnMissing: true }],
+		}),
+	],
+};
